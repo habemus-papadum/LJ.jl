cf7e0d5d7ab85625654d3868bc6040adf0071a63
Nehal Patel <nehal@alum.mit.edu>
2025-05-18 16:29:11 -0400

    sweep hue

--- scribbles/CudaMay25/pluto.jl
+++ scribbles/CudaMay25/pluto.jl
@@ -27,6 +27,9 @@ end
 # ╔═╡ 04f0e217-2498-40c8-8f6a-93f7d71613f0
 
 
+# ╔═╡ 1a871075-c738-495b-9fe1-14b9d91b88e8
+@bind hue Slider(0:360, 7, true)
+
 # ╔═╡ e865478b-7892-40a8-87b0-a7fe64adc5cc
 begin
 	struct Literal{T} end
@@ -60,7 +63,9 @@ begin
 end
 
 # ╔═╡ 4aacf788-6536-4c4b-a8e3-774e16ed2a12
+begin 
 @bind x Slider(100:600, 400, true)
+end
 
 # ╔═╡ e4f0003f-b72e-4307-aef9-ccd60b177073
 let 
@@ -72,7 +77,28 @@ let
     b
 end
 
+# ╔═╡ 08e8c8b9-a79e-40d7-af1c-1fc84b456817
+let 
+	function sweep(hue) 
+	    hr = deg2rad(hue) 
+	    c  = cos(hr)
+	    s  = sin(hr)
+	    (i,j,w,h) -> RGB{Float32}(Lab(100*i/w, 100*j/h*c, 100*j/h*s))
+	end
+    n = x
+    b = CuArray{RGB{Float32}}(undef, n,n)
+    generate(sweep(hue), b)
+    b
+end
+    
+
+# ╔═╡ 3870a643-1b43-492b-9ba3-7d8eeaeca42a
+@bind rad Slider(0.1f0:0.1f0:2.0f0, 1.7f0, true)	
+
+
 # ╔═╡ 13a58ea5-e23d-4a65-b7e2-b57dfad504d8
+begin
+const _rad = rad
 shader(i,j, r, c) = begin
         i, ii = divrem(i, 100i32)
         j, jj = divrem(j, 100i32)
@@ -82,10 +108,10 @@ shader(i,j, r, c) = begin
 
         x  =  Float32(ii - 50i32) / 50.0f0
         y  =  Float32(jj - 50i32) / 50.0f0
-        x = abs(x)
-        y = abs(y)
-        if x  + y - 1.7f0 < 0.0f0
-        	if max(x,y) > 0.98
+        x = Float32(abs(x))
+        y = Float32(abs(y))
+        if x  + y - _rad < 0.0f0
+        	if max(x,y) > 0.98f0
                 return colorant"black"
             else
                 return colorant"salmon"
@@ -93,6 +119,7 @@ shader(i,j, r, c) = begin
         else 
         	return colorant"steelblue"
         end
+end
 end
 
 # ╔═╡ eb07dfb9-c511-4132-8caa-b1c6800f8c7b
@@ -107,7 +134,10 @@ end
 # ╠═8db812b4-820a-4291-b8c0-b3dc29b526df
 # ╠═04f0e217-2498-40c8-8f6a-93f7d71613f0
 # ╠═e4f0003f-b72e-4307-aef9-ccd60b177073
+# ╠═1a871075-c738-495b-9fe1-14b9d91b88e8
+# ╠═08e8c8b9-a79e-40d7-af1c-1fc84b456817
 # ╠═e865478b-7892-40a8-87b0-a7fe64adc5cc
 # ╠═4aacf788-6536-4c4b-a8e3-774e16ed2a12
 # ╠═eb07dfb9-c511-4132-8caa-b1c6800f8c7b
 # ╠═13a58ea5-e23d-4a65-b7e2-b57dfad504d8
+# ╠═3870a643-1b43-492b-9ba3-7d8eeaeca42a
